64fa90ce93755e3b75a655ae0c4871327973150a
KOR-Gsu <jp1507@naver.com>
2020-04-07 13:21:54 +0900

    1

--- 제출/Chapter2/문제1/문제1/Listmain.cpp
+++ 제출/Chapter2/문제1/문제1/Listmain.cpp
@@ -1,17 +1,23 @@
 #pragma once
 #include "ArrayList.h"
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
+
+#define LISTSIZE 100
 
 void main()
 {
+	srand((unsigned)time(NULL));
+
 	List* list = new List;
 	LData num;
 	int sum = 0;
 
 	ListInit(list);
 
-	for (int i = 1; i <= 9; i++)
+	for (int i = 1; i <= LISTSIZE; i++)
 		LInsert(list, i);
 
 	if (LFirst(list, &num))
@@ -23,6 +29,40 @@ void main()
 	}
 
 	cout << "리스트 전체 수의 합 : " << sum << endl;
+
+	int shuf;
+	int pos;
+	cout << "리스트를 섞을 횟수를 입력하시오 : ";
+	cin >> shuf;
+
+
+	for (int i = 0; i < shuf; i++)
+	{
+		for (int j = 0; j < LISTSIZE; j++)
+		{
+			pos = rand() % LISTSIZE;
+
+			LFirst(list, &num);
+
+			for (int k = 0; k < pos; k++)
+				LNext(list, &num);
+
+			LInsert(list, LRemove(list));
+		}
+	}
+
+	system("cls");
+
+	cout << "리스트 전체 데이터 출력" << endl;
+
+	if (LFirst(list, &num))
+	{
+		cout << num << endl;
+
+		while (LNext(list, &num))
+			cout << num << endl;
+	}
+
 
 	if (LFirst(list, &num))
 	{
@@ -37,7 +77,10 @@ void main()
 	}
 
 
-	cout << "리스트 전체 데이터 출력" << endl;
+	system("pause");
+	system("cls");
+
+	cout << "2와 3의 배수를 삭제한 리스트 전체 데이터 출력" << endl;
 
 	if (LFirst(list, &num))
 	{

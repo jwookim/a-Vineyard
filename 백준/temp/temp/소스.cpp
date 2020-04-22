4b0b4f906059ac2e9c1b5f8190e96573929c640e
KOR-Gsu <jp1507@naver.com>
2020-04-22 15:00:08 +0900

    1

--- 백준/temp/temp/소스.cpp
+++ 백준/temp/temp/소스.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int calc(int k, int n)
+{
+	if (k == 0)
+		return n;
+	else
+	{
+		int sum = 0;
+		for (int i = 1; i <= n; i++)
+			sum += calc(k - 1, i);
+		return sum;
+	}
+}
+
 int main()
 {
-	int A, B, V;
-	cin >> A >> B >> V;
+	int t;
+	cin >> t;
+	int* k = new int[t];
+	int* n = new int[t];
 
-	if ((V - A) % (A - B) == 0)
-		cout << (V - A) / (A - B) + 1;
-	else
-		cout << (V - A) / (A - B) + 2;
-	return 0;
+	for (int i = 0; i < t; i++)
+		cin >> k[i] >> n[i];
+
+	for (int i = 0; i < t; i++)
+		cout << calc(k[i], n[i]) << endl;
+	delete[] k, n;
 }

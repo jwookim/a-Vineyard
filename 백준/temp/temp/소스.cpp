1964abc752e6cc5d146ad2b2ddb461ea6e4abb30
KOR-Gsu <jp1507@naver.com>
2020-04-23 15:14:09 +0900

    1

--- 백준/temp/temp/소스.cpp
+++ 백준/temp/temp/소스.cpp
@@ -1,30 +1,34 @@
-#include<iostream>
-using namespace std;
-
-int calc(int k, int n)
-{
-	if (k == 0)
-		return n;
-	else
-	{
-		int sum = 0;
-		for (int i = 1; i <= n; i++)
-			sum += calc(k - 1, i);
-		return sum;
-	}
-}
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-	int t;
-	cin >> t;
-	int* k = new int[t];
-	int* n = new int[t];
+	int m, n;
+	scanf_s("%d %d", &m, &n);
 
-	for (int i = 0; i < t; i++)
-		cin >> k[i] >> n[i];
+	bool* eratos = (bool*)malloc(sizeof(bool) * n);
 
-	for (int i = 0; i < t; i++)
-		cout << calc(k[i], n[i]) << endl;
-	delete[] k, n;
+	for (int i = 0; i < n; i++)
+		eratos[i] = true;
+
+	eratos[0] = false;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (eratos[i - 1] == false)
+			continue;
+
+		for (int j = 2; j*i <= n; j++)
+			eratos[i*j - 1] = false;
+	}
+
+	for (; m <= n; m++)
+	{
+		if (eratos[m - 1] == true)
+			printf("%d\n", m);
+	}
+
+	free(eratos);
+
+	return 0;
 }

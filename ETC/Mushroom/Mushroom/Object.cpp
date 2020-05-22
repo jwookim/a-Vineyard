23bbe0559984e9b669edadaaa4b342bc15d73db8
Kimminguy0453! <astora0453@gmail.com>
2020-05-22 14:55:53 +0900

    1

--- ETC/Mushroom/Mushroom/Object.cpp
+++ ETC/Mushroom/Mushroom/Object.cpp
@@ -1,1 +1,12 @@
 #include "Object.h"
+
+
+void Object::Draw()
+{
+	DrawManager.DrawPoint(to_string(m_chShape), m_Position.x, m_Position.y);
+}
+
+void Object::Erase()
+{
+	DrawManager.ErasePoint(m_Position.x, m_Position.y);
+}
--- ETC/Mushroom/Mushroom/StageManage.h
+++ ETC/Mushroom/Mushroom/StageManage.h
@@ -1,4 +1,5 @@
 #pragma once
+#include"List.h"
 #include"Block.h"
 class StageManage
 {

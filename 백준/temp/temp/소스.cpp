3a64508ed4fa4f58a472a89be06965e91518e8f4
jwookim <xodlf6604@naver.com>
2020-07-09 14:59:56 +0900

    1

--- 백준/temp/temp/소스.cpp
+++ 백준/temp/temp/소스.cpp
@@ -1,41 +1,26 @@
 #include<iostream>
-#include<math.h>
+#include<Windows.h>
 using namespace std;
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int star(int num)
+{
+	cout << "***\n* *\n***";
+	return 1;
+}
 
 int main()
 {
-	int t, d;
-	int x1, y1, r1, x2, y2, r2;
-	int* n;
+	int num;
 
-	cin >> t;
+	cin >> num;
 
-	n = new int[t];
-
-	for (int i = 0; i < t; i++)
-	{
-		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-
-		if (x1 == x2 && y1 == y2 && r1 == r2)
-		{
-			n[i] = -1;
-			continue;
-		}
-
-		d = pow(x1 - x2, 2) + pow(y1 - y2, 2);
-
-		if (pow(r1 + r2, 2) == d || pow(r1 - r2, 2) == d)
-			n[i] = 1;
-		else if (pow(r1 + r2, 2) < d || pow(r1 - r2, 2) < d)
-			n[i] = 2;
-		else
-			n[i] = 0;
-	}
-
-	for (int i = 0; i < t; i++)
-		cout << n[i] << endl;
-
-	delete[] n;
-
+	
+	cout << star(num);
 	return 0;
 }

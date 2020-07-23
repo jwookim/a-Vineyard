9c48511f790eb08bc22faa8b389ab64bb0be92f7
jwookim <xodlf6604@naver.com>
2020-07-23 14:35:20 +0900

    1

--- 제출/WinAPI/Project2/Project2/소스.cpp
+++ 제출/WinAPI/Project2/Project2/소스.cpp
@@ -1,9 +1,18 @@
 #include<Windows.h>
+#include<math.h>
+#include<ctime>
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;//글로벌 인스턴스핸들값
+
+double GetRadian(double degree)
+{
+	return degree * (3.1415926535 / (double)180);
+}
+
 LPCTSTR lpszClass = TEXT("HelloWorld"); //클래스 명 : 창이름
 	int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
 {
+		srand((unsigned)time(NULL));
 	HWND hWnd;
 	MSG Message;
 	WNDCLASS WndClass;
@@ -33,6 +42,7 @@ LPCTSTR lpszClass = TEXT("HelloWorld"); //클래스 명 : 창이름
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
+	PAINTSTRUCT ps;
 	switch (iMessage)
 	{
 	case WM_DESTROY: // 윈도우가 파괴되었다는 메세지
@@ -40,8 +50,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		return 0; //WndProc의 Switch는 break 대신 return 0; 를 쓴다.
 	case WM_LBUTTONDOWN:
 		hdc = GetDC(hWnd);
+		SetTextAlign(hdc, TA_CENTER);
 		TextOut(hdc, 100, 100, TEXT("^오^"), 4);
 		ReleaseDC(hWnd, hdc);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		for (int y = 0; y <= 100; y += 10)
+		{
+			for (int i = 0; i < 90000; i++)
+			{
+				SetPixel(hdc, 500 + cos(GetRadian(i / 1000)) * 100, 500 + sin(GetRadian(i / 1000)) * y, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 - cos(GetRadian(i / 1000)) * 100, 500 + sin(GetRadian(i / 1000)) * y, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 + cos(GetRadian(i / 1000)) * 100, 500 - sin(GetRadian(i / 1000)) * y, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 - cos(GetRadian(i / 1000)) * 100, 500 - sin(GetRadian(i / 1000)) * y, RGB(rand() % 266, rand() % 266, rand() % 266));
+			}
+		}
+		for (int x = 0; x <= 100; x += 10)
+		{
+			for (int i = 0; i < 90000; i++)
+			{
+				SetPixel(hdc, 500 + cos(GetRadian(i / 1000)) * x, 500 + sin(GetRadian(i / 1000)) * 100, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 - cos(GetRadian(i / 1000)) * x, 500 + sin(GetRadian(i / 1000)) * 100, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 + cos(GetRadian(i / 1000)) * x, 500 - sin(GetRadian(i / 1000)) * 100, RGB(rand() % 266, rand() % 266, rand() % 266));
+				SetPixel(hdc, 500 - cos(GetRadian(i / 1000)) * x, 500 - sin(GetRadian(i / 1000)) * 100, RGB(rand() % 266, rand() % 266, rand() % 266));
+			}
+		}
+		EndPaint(hWnd, &ps);
 		return 0;
 	}
 	return(DefWindowProc(hWnd, iMessage, wParam, lParam)); // case에 있는 메시지를 제외한 나머지 메시지를 처리한다.
